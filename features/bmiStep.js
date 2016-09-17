60b7e230763d2db666c4e8d58d884526b70fbd08
alincode <alin.code@gmail.com>
2016-09-17 22:56:06 +0800

    copy and paste generate meesage

--- features/bmiStep.js
+++ features/bmiStep.js
@@ -1,3 +1,22 @@
 module.exports = function() {
 
+  this.Given(/^height is "([^"]*)"$/, function(arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^weight is "([^"]*)"$/, function(arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.When(/^calculator add two number$/, function(callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Then(/^result should be "([^"]*)"$/, function(arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
 };
